4a6f1376e3780846f5069cedf1494b9124ac4405
Marlon Marques <marlon@dell21.(none)>
2014-02-20 10:37:35 -0300

    Finish Layout and Routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,43 +2,36 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+subject { page }
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content 'C3 App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('C3 App')
-    end
 
-it "should have the right title" do
-  visit '/static_pages/home'
-  expect(page).to have_title("Ruby on Rails Tutorial C3 App | Home")
-end
+    it { should have_content('C3 App') }
+    it { should have_title("Ruby on Rails Tutorial C3 App") }
+    it { should_not have_title('| Home') }
 
   end
 
 describe "Help page" do
+  before { visit help_path }
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the right title" do
-  visit '/static_pages/help'
-  expect(page).to have_title("Ruby on Rails Tutorial C3 App | Help")
+  it { should have_content('Help')  }
+  it { should have_title('Ruby on Rails Tutorial C3 App | Help')}
 end
 
-  end
+  
 
    describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
 
     it "should have the right title" do
-  visit '/static_pages/about'
+  visit about_path
   expect(page).to have_title("Ruby on Rails Tutorial C3 App | About Us")
 end
 
@@ -47,12 +40,12 @@ end
   describe "Contact Page" do
 
     it "should have the content 'Contact'" do
-        visit '/static_pages/contact'
+        visit contact_path
         expect(page).to have_content('Contact')
     end
 
     it "should have the right title" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("Contact") 
 
     end
